fa7d6110c224136860e38c4bf9de783c7fcf9d22
Jones291728 <77008226+Jones291728@users.noreply.github.com>
2021-01-18 12:05:52 +0800

    Add files via upload

---
+++ D01 numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+#1.生成一個等差數列，首數為0，尾數為20，公差為1的數列。
+case01=np.arange(0,20,1)
+print(case01)
+#2.呈上題，將以上數列取出偶數。
+case02=np.arange(0,20,2)
+print(case02)
+#3.呈1題，將數列取出3的倍數。
+case03=np.arange(0,20,3)
+print(case03)
